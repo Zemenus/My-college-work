568c0049b69718922688fd1f31ba4f6d5f0bdf38
Zemenus <horror.semen@mail.ru>
2023-10-20 10:38:21 +0300

    Добавил work1

---
+++ work1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+	if (num <= 1) {
+		return false;
+	}
+	for (int i = 2; i * i <= num; i++) {
+		if (num % i == 0) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+}
+
+int main() {
+
+	setlocale(0, "");
+
+	int num;
+	bool isOn = true;
+
+	while (isOn = true) {
+
+		cout << "Введите число: " << endl;
+		cin >> num;
+
+		if (isPrime(num)) {
+			cout << num << " является простым числом" << endl;
+		}
+		else {
+			cout << num << " не является простым числом" << endl;
+		}
+
+	}
+
+	return 0;
+}
