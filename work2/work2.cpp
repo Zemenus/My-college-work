6ffd28235c37f19a277ae004baf16eccee267e06
Zemenus <horror.semen@mail.ru>
2023-10-23 09:51:35 +0300

    Добавьте файлы проекта.

---
+++ work2/work2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+	setlocale(0, "");
+	
+	int number;
+	cout << "Введите число: " << endl;
+	cin >> number;
+
+	if (number > 0) {
+		cout << "Число " << number << " положительное" << endl;
+	}
+	else if (number < 0) {
+		cout << "Число " << number << " отрицательное" << endl;
+	}
+	else {
+		cout << "Число " << number << " ноль" << endl;
+	}
+
+	return 0;
+}
