b06dd81129b45273154c4c5c08552be1677c3733
Zemen <horror.semen@gmail.com>
2023-10-23 10:33:54 +0300

    Initial commit

---
+++ work3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+	setlocale(0, "");
+
+	int year;
+	cout << "Введите год: ";
+	cin >> year;
+
+	if (year % 4 == 0) {
+		if (year % 100 == 0) {
+			if (year % 400 == 0) {
+				cout << year << " високосный год" << endl;
+			}
+			else {
+				cout << year << " не високосный год" << endl;
+			}
+		}
+		else {
+			cout << year << " високосный год" << endl;
+		}
+	}
+	else {
+		cout << year << " не високосный год" << endl;
+	}
+
+	return 0;
+}
